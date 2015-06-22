785b419f3973878b6c20aa4ccf0b9ac116f7119b
Kristian Damian <kristiandamian@gmail.com>
2015-06-22 10:31:34 -0600

    Prueba de audio jack en android
    pruebas de lectura de datos por el jack de audio (audifonos) en android
    con xamarin

---
+++ NoisyAudioStreamReceiver.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+
+namespace PruebaJackAudio
+{
+	[BroadcastReceiver]
+	public class NoisyAudioStreamReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Intent intent)
+		{
+			if (Intent.ActionMediaButton.Equals (intent.Action)) {
+				Toast.MakeText (context, "Received intent ActionMediaButton!", ToastLength.Short).Show ();
+			}
+			if (AudioManager.ActionAudioBecomingNoisy.Equals (intent.Action)) {
+				Toast.MakeText (context, "Received intent ActionAudioBecomingNoisy!", ToastLength.Short).Show ();
+			}
+		}
+	}
+}
+
