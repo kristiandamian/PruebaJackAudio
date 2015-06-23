8bf5cffd3a225ecfcbfd223f48c87b7fa519f851
Kristian Damian <kristiandamian@gmail.com>
2015-06-22 22:17:11 -0600

    Se agrega para ver el número de boton que se lee en el pulso

--- NoisyAudioStreamReceiver.cs
+++ NoisyAudioStreamReceiver.cs
@@ -24,6 +24,19 @@ namespace PruebaJackAudio
 			if (Intent.ActionMediaButton.Equals (intent.Action)) {
 				Toast.MakeText (context, "Se presiono el boton del manos libres!", ToastLength.Short).Show ();
 			}
+			var keyEvent = (KeyEvent)intent.GetParcelableExtra(Intent.ExtraKeyEvent);
+
+			switch (keyEvent.KeyCode)
+			{
+			case Keycode.MediaPlay:
+				break;
+			case Keycode.MediaPlayPause:
+				break;
+			case Keycode.MediaNext:
+				break;
+			case Keycode.MediaPrevious:
+				break;
+			}
 			if (AudioManager.ActionAudioBecomingNoisy.Equals (intent.Action)) {
 				Toast.MakeText (context, "Received intent ActionAudioBecomingNoisy!", ToastLength.Short).Show ();
 			}
